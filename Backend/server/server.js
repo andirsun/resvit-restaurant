30ae5b921665439ef22d36ba14362863a7181b7a
andirsun <ander.laverde.dev@gmail.com>
2019-09-29 22:26:39 -0500

    authenticated function

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -2,18 +2,15 @@ require('./config/config');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-var sqlite3 = require('sqlite3').verbose()
+const sqlite3 = require('sqlite3').verbose()
 
-
+////Create or read the Database file//////
 var db = new sqlite3.Database('prueba1');
+//////////////////////////////////////////
 
-db.serialize(function(){ 
-  db.each("SELECT id FROM users", function(err, row) {
-      console.log("User id : "+row.id);
-  });
- 
-});
-db.close();
+
+
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,8 +21,30 @@ app.use(bodyParser.json());
 /////////
 
 
-//========================================================
-/*
+//=================Events=======================================
+app.get('/authenticate',function(req,res){
+  let body = req.body;//Body request
+  if(body.id==undefined){
+    req.status(400).json({
+      ok:false,
+      response:1,
+      content:'User Id is require for petition'
+    });
+  }else{
+    ///I need to put the query of autentication here
+    db.serialize(function(){ 
+      db.each("SELECT id FROM users", function(err, row) {
+          console.log("User id : "+row.id);
+      });
+    });
+    res.json({
+      user:id
+    });
+  }
+});
+
+
+
 app.post('/usuario',function(req,res){
     let body = req.body;//asi leo lo que hay en el vody de la peticion post, debe usarse body parser de npm
 
@@ -41,7 +60,8 @@ app.post('/usuario',function(req,res){
     }
     
 
-});*/
+});
+//===========================================================
 app.post('/postevent',function(req,res){
   res.json('Aca va a tener que enviarme la informacion del evento para crearlooooo');
 });
@@ -54,7 +74,7 @@ app.get('/getevent/:id',function(req,res){
       idEvent:id
   });
 });
-
+db.close();////always need to close de db
 
 app.listen(process.env.PORT,()=>{
     console.log('escuchando al puerto',3000);
