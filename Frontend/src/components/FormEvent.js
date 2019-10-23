673770625233d4772f70d5b66161df2d1a3c298a
YanCarlosCertucheGrueso <yancarlos@javerianacali.edu.co>
2019-10-22 23:30:56 -0500

    add datepicker

--- Frontend/src/components/FormEvent.js
+++ Frontend/src/components/FormEvent.js
@@ -1,21 +1,41 @@
 import React, {Component} from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react'
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 export class FormEvent extends Component{
+
+    state = {
+        startDate: new Date()
+    };
+     
+    handleChange = date => {
+        this.setState({
+          startDate: date
+        });
+    };
     
     render(){
         return(
             <div>
-                <Segment inverted>
-                    <Form inverted>
-                    <Form.Group widths='equal'>
-                        <Form.Input fluid label='First name' placeholder='First name' />
-                        <Form.Input fluid label='Last name' placeholder='Last name' />
-                    </Form.Group>
-                    <Form.Checkbox label='I agree to the Terms and Conditions' />
-                    <Button type='submit'>Submit</Button>
-                    </Form>
-                </Segment>               
+                <Form>
+                    <Form.Field>
+                        <label>Nombre de Evento</label>
+                        <input placeholder='Nombre del Evento' />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Tipo</label>
+                        <input placeholder='Tipo del evento ej. teatro, musica' />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Fecha</label>
+                        <DatePicker
+                            selected={this.state.startDate}
+                            onChange={this.handleChange}/>
+                    </Form.Field>
+                    <Button className='ui inverted secondary button' type='submit'>Submit</Button>
+                </Form>              
             </div>
         )
     }
