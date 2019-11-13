1b57868b6a59a50e6d6dbf9a96b8f39dc38e9f19
Yan Carlos Certuche Grueso <yancertuche@gmail.com>
2019-11-13 15:47:23 -0500

    add style to add Users

--- Frontend/src/components/FormRestaurat.js
+++ Frontend/src/components/FormRestaurat.js
@@ -3,6 +3,11 @@ import { Form, Input, TextArea, Button, Segment } from 'semantic-ui-react'
 export class FormRestaurant extends Component{
 
     render(){
+        const options=[
+            {key: 2, text: 'Usuario Restaurante', value : 2},
+            {key: 1, text: 'Usuario Comensal', value : 1},
+            {key: 3, text: 'Usuario Administrador', value : 3}
+        ]
         return(
             <Segment>
                 <Form>
@@ -10,15 +15,21 @@ export class FormRestaurant extends Component{
                 <Form.Field
                     id='form-input-control-first-name'
                     control={Input}
-                    label='First name'
+                    label='Nombre Usuario'
                     placeholder='First name'
                 />
                 <Form.Field
                     id='form-input-control-last-name'
                     control={Input}
-                    label='Last name'
+                    label='Contraseña'
                     placeholder='Last name'
                 />
+                <Form.Select
+                    fluid
+                    label='Tipo Usuario'
+                    options={options}
+                    placeholder='ej.Usuario Restaurante'
+                />                
                 </Form.Group>
                 <Form.Field
                 id='form-textarea-control-opinion'
--- Frontend/src/pages/AddRestaurant.js
+++ Frontend/src/pages/AddRestaurant.js
@@ -3,14 +3,24 @@ import {FormRestaurant} from '../components/FormRestaurat'
 import {Title} from '../components/Title'
 import {Link} from 'react-router-dom'
 import { Button} from 'semantic-ui-react'
+import {MenuProfile} from '../components/MenuProfile'
+import logo from '../images/RevitBlanco.png';
+import '../styles/menu.css';
+import '../styles/addRestaurantStyle.css'
 
 
 export class AddRestaurant extends Component{
     render(){
         return(
             <div>
+                <header className="App-header">
+                <div className="Menu">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <MenuProfile></MenuProfile>
+                </div>
+                </header>
                 <div className="ui bottom attached button">
-                    <Title>Añadir Usuario Restaurante</Title>
+                    <Title>Añadir Usuario</Title>
                     <div>
                         <Link to ='/AddDecoration' >
                             <Button className='ui inverted secondary button' >
@@ -20,7 +30,7 @@ export class AddRestaurant extends Component{
                         </Link>
                     </div>
                 </div>
-                <div>
+                <div className="containerForm">
                     <FormRestaurant>
                     </FormRestaurant>
                 </div>
